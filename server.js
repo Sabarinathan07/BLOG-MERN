88371ccbf35d4ae3e3caf681d4a5fdcaa7089d5a
Sabari <r.sabarinathan22@gmail.com>
2021-07-23 15:46:05 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req,res)=> res.send('Api working!!'))
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
