b9831efeb504dca876761a13e819c2d5beaa2eb8
Sabari <r.sabarinathan22@gmail.com>
2021-07-26 00:25:38 +0530

    profile complete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+const config = require('config');
+const axios = require('axios');
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
@@ -292,5 +295,61 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 	}
 });
 
+// // @route DELETE api/profile/github/:username
+// // @description get user repos from github
+// // @access Public
+// router.get('/github/:username', (req, res) => {
+//     try {
+//         const options = {
+//             uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+//             method: 'GET',
+//             headers: {'user-agent':'node.js'}
+//         }
+
+//         request(options, (error, response, body) => {
+//             if (error) return console.log(error);
+//             if (response.statusCode !== 200) {
+//                 return res.status(400).json({msg:'No Github profile found'})
+//             }
+
+//             res.json(JSON.parse(body))
+//         })
+
+        
+//     } catch (err) {
+//         console.error(err.message);
+//         res.status(500).send('Github Error');
+        
+//     }
+// })
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+router.get('/github/:username', (req, res) => {
+    try {
+      const options = {
+        uri: encodeURI(`https://api.github.com/users/${
+          req.params.username
+        }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+          'githubClientId'
+        )}&client_secret=${config.get('githubSecret')}`),
+        method: 'GET',
+        headers: { 'user-agent': 'node.js' }
+      };
+  
+      request(options, (error, response, body) => {
+        if (error) console.error(error);
+  
+        if (response.statusCode !== 200) {
+          return res.status(404).json({ msg: 'No Github profile found' });
+        }
+  
+        res.json(JSON.parse(body));
+      });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Git hub Error');
+    }
+  });
 
 module.exports = router;
