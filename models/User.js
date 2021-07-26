84784d5ba4480d1d210ea9e133b1a369dd5240a3
Sabari <r.sabarinathan22@gmail.com>
2021-07-26 16:37:07 +0530

    backend complete

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
 	name: {
 		type: String,
 		required: true,
